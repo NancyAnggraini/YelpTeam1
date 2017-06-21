2fcaf20183afefb46931837799a9b4a0ac5fba00
Miguel Perello garcia <canyavall@gmail.com>
2017-06-21 17:38:33 +0200

    Testing domain and service

--- Backend/yelp/src/test/java/yelp/repository/RestaurantRepositoryTest.java
+++ Backend/yelp/src/test/java/yelp/repository/RestaurantRepositoryTest.java
@@ -11,11 +11,13 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import yelp.AbstractYelpIntegrationTests;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Transactional
 @Sql("/test-data.sql")
-public class RestaurantRepositoryTest {
+public class RestaurantRepositoryTest extends AbstractYelpIntegrationTests{
 
 	@Autowired
 	RestaurantRepository repository;
@@ -23,5 +25,10 @@ public class RestaurantRepositoryTest {
 	@Test
 	public void count() {
 		assertThat(repository.count()).isEqualTo(3);
+	}
+	
+	@Test
+	public void findById(){
+		assertThat(repository.findById(1L).getName()).isEqualTo("testRestaurantName");
 	}
 }
