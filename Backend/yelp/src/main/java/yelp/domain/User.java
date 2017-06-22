74a2977f06e9aa260daa959b27516635fde6894c
Miguel Perello garcia <canyavall@gmail.com>
2017-06-22 14:27:11 +0200

    user and usertests finished

--- Backend/yelp/src/main/java/yelp/domain/User.java
+++ Backend/yelp/src/main/java/yelp/domain/User.java
@@ -1,5 +1,6 @@
 package yelp.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,15 +13,36 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
+@EqualsAndHashCode(exclude = {"password", "id"})
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private Long id;
 	
+	@Column(name = "first_name", nullable = false, length = 30)
 	private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 45)
 	private String lastName;
+	
+	@Column(nullable = false, length = 75)
 	private String email;
+	
+	@Column(nullable = false, length = 76)
 	private String password;
+	
+	@Column(nullable = false, length = 75)
 	private String token;
+	
+	public User(){
+		/* default constructor: required by JPA */
+	}
+	
+	public User (String firstName, String lastName, String email, String password, String token){
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.token = token;
+	}
 }
--- Backend/yelp/src/test/java/yelp/service/RestaurantServiceTest.java
+++ Backend/yelp/src/test/java/yelp/service/RestaurantServiceTest.java
@@ -3,19 +3,11 @@ package yelp.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@Transactional
-@Sql("/test-data.sql")
-public class RestaurantServiceTest {
+import yelp.AbstractYelpIntegrationTests;
+
+public class RestaurantServiceTest extends AbstractYelpIntegrationTests {
 
 	@Autowired
 	RestaurantService service;
