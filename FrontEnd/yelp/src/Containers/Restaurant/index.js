fc0a6a0e4ac84cef57efc71b6bbc4d882be08957
Manel <manelpavon10@gmail.com>
2017-06-23 14:35:24 +0200

    Update index.js

--- FrontEnd/yelp/src/Containers/Restaurant/index.js
+++ FrontEnd/yelp/src/Containers/Restaurant/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../style.css';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Navbar from '../Navbar';
 import ReviewsList from '../../Components/ReviewsList';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import HalfStar from 'material-ui/svg-icons/toggle/star-half';
